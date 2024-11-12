7b1d92d83ecf21b39a4e4f5db10666db518ece20
achrafmechhour <achrafmechhour01@gmail.com>
2024-11-12 09:23:38 +0100

    fix code command synccommand

--- src/Command/SyncCommand.php
+++ src/Command/SyncCommand.php
@@ -14,78 +14,238 @@ use Symfony\Bundle\FrameworkBundle\Console\Application;
 class SyncCommand extends Command
 {
     protected static $defaultName = 'run:sync';
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        parent::__construct();
+        $this->entityManager = $entityManager; // Initialize the entity manager
+    }
 
     protected function configure(): void
     {
         $this->setDescription('Synchronizes data from MySQL to SQL Server.');
     }
+    private function initializeSynchronization(): SynchronisationInfo
+    {
+        $synchronisation = new SynchronisationInfo();
+        $synchronisation->setDateStart(new \DateTime());
+        $this->entityManager->persist($synchronisation);
+        $this->entityManager->flush();
+        return $synchronisation;
+    }
+    private function initializeConnections(): void
+    {
+        $doctrine = $this->getApplication()->getKernel()->getContainer()->get('doctrine');
+        $defaultEntityManager = $doctrine->getManager();
+        $ugouvEntityManager = $doctrine->getManager('ugouv');
 
+        $this->sqlServerConnection = $defaultEntityManager->getConnection();
+        $this->mysqlConnection = $ugouvEntityManager->getConnection();
+    }
+    private function extractPrimaryKeyValues(array $data, array $primaryKey): array
+    {
+        $columnIds = [];
+
+        foreach ($data as $row) {
+            $keyValues = [];
+
+            foreach ($primaryKey as $keyColumn) {
+                // Determine if it's a composite key (array format) or single key (string)
+                $columnName = is_array($keyColumn) && isset($keyColumn['ColumnName']) ? $keyColumn['ColumnName'] : $keyColumn;
+                $keyValues[] = $row[$columnName] ?? null;
+            }
+
+            // Join composite key values with a dash; for single keys, it just contains one value
+            $columnIds[] = implode('-', $keyValues);
+        }
+
+        return $columnIds;
+    }
+
+    /**
+     * Handles synchronization errors by logging and updating the SynchronisationInfo record.
+     */
+    private function handleSynchronizationError(SynchronisationInfo $synchronisation, string $tableName, \Exception $e): void
+    {
+        $status = 'error in Table ' . ($tableName ?: 'N/A');
+        $message = $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine();
+
+        // Use the existing updateSyncInfo method
+        $this->updateSyncInfo($synchronisation, $status, $message);
+    }
+    private function getAllTableNames(): array
+    {
+        $sql = "SELECT TABLE_NAME FROM information_schema.tables
+                WHERE TABLE_TYPE = 'BASE TABLE'
+                AND TABLE_SCHEMA = 'ugouv'
+                AND TABLE_NAME NOT IN ('user_created_id','_biomed','_biomed_14_09_22','_biomed_15_09_22_mod','umouvement_antenne_')";
+
+        $stmt = $this->sqlServerConnection->prepare($sql);
+        return $stmt->executeQuery()->fetchAllAssociative();
+//        dd($stmt->executeQuery()->fetchAllAssociative());
+    }
+//execute function
+//    protected function execute(InputInterface $input, OutputInterface $output): int
+//    {
+//        set_time_limit(0); // 0 means no limit
+//        ini_set('memory_limit', '-1'); // '-1' means unlimited memory
+//
+//        $tableName = '';
+//        try {
+//            $synchronisation = new SynchronisationInfo();
+//            $synchronisation->setDateStart(new \DateTime());
+//            $this->entityManager->persist($synchronisation);
+//            $this->entityManager->flush();
+//
+//            $output->writeln('Starting data synchronization...');
+//
+//            // Get the default and 'ugouv' entity managers
+//            $doctrine = $this->getApplication()->getKernel()->getContainer()->get('doctrine');
+//            $defaultEntityManager = $doctrine->getManager();
+//            $ugouvEntityManager = $doctrine->getManager('ugouv');
+//
+//            // Retrieve DBAL connections
+//            $this->sqlServerConnection = $defaultEntityManager->getConnection();
+//            $this->mysqlConnection = $ugouvEntityManager->getConnection();
+//
+//            $tables = $this->getAllTableNames();
+////            dd($tables);
+//            $tableCount = 1;
+//            foreach ($tables as $table) {
+//                $tableName = $table['TABLE_NAME'];
+//                $tableName = '_biomed_14_09_22_(2)';
+//
+//                $output->writeln("$tableCount Processing table: $tableName");
+//
+//                $moreData = true;
+//                $limit = 5000;
+//                $tableCount++;
+//
+//                while ($moreData) {
+//                    $data = $this->fetchUnsynchronizedData($tableName, $limit);
+//                    if (!empty($data)) {
+//                        $primaryKey = $this->getIdOrPrimaryKey($tableName);
+////                            dd($primaryKey);
+//                        if (!$primaryKey) {
+//                            throw new \Exception("Table $tableName does not contain an 'id' column or primary key.");
+//                        }
+//                        // Check if we are dealing with a composite primary key
+//                        if (is_array($primaryKey)) {
+//                            // Composite key case: build an array of composite key values
+//                            $columnIds = [];
+//                            foreach ($data as $row) {
+//                                $compositeKey = [];
+//
+//                                foreach ($primaryKey as $keyColumn) {
+//                                    // Check if $keyColumn is an array or a string
+//                                    if (is_array($keyColumn) && isset($keyColumn['ColumnName'])) {
+//                                        // If $keyColumn is an array, use the 'ColumnName' key
+//                                        $compositeKey[] = $row[$keyColumn['ColumnName']];
+//                                    } else {
+//                                        // If $keyColumn is a string, use it directly
+//                                        $compositeKey[] = $row[$keyColumn];
+//                                    }
+//                                }
+//
+//                                // Concatenate composite key values with a separator (e.g., a dash or comma)
+//                                $columnIds[] = implode('-', $compositeKey);
+//                            }
+//
+//
+//                        } else {
+//                            // Single primary key case
+//                            $columnIds = array_column($data, $primaryKey);
+//                        }
+//
+//
+//                        $this->upsertDataIntoTable($data, $tableName, $primaryKey);
+//                        $this->flagDataAsSynchronized($tableName, $primaryKey, $data);
+//                    } else {
+//                        $moreData = false;
+//                    }
+//                }
+//                gc_collect_cycles();
+//            }
+//
+//            $output->writeln('Data synchronized successfully!');
+//            $this->updateSyncInfo($synchronisation, 'success', 'Synchronization completed successfully.');
+//            return Command::SUCCESS;
+//
+//        } catch (\Exception $e) {
+//            $output->writeln('Error with table ' . ($tableName ?: 'N/A') . ': ' . $e->getMessage());
+//
+//            // Log the error and store it in the synchronization record
+//            $this->updateSyncInfo(
+//                $synchronisation,
+//                'error in Table ' . ($tableName ?: 'N/A'),
+//                $e->getMessage() . ' in file ' . $e->getFile() . ' on line ' . $e->getLine()
+//            );
+//            return Command::FAILURE;
+//        }
+//    }
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
+        // Set unlimited execution time and memory limit
+        set_time_limit(0);
+        ini_set('memory_limit', '-1');
+
+        $tableName = '';
         try {
+            $synchronisation = $this->initializeSynchronization();
+
             $output->writeln('Starting data synchronization...');
-
-            // Get the default and 'ugouv' entity managers
-            $doctrine = $this->getApplication()->getKernel()->getContainer()->get('doctrine');
-            $defaultEntityManager = $doctrine->getManager();
-            $ugouvEntityManager = $doctrine->getManager('ugouv');
-
-            // Retrieve DBAL connections
-            $this->sqlServerConnection = $defaultEntityManager->getConnection();
-            $this->mysqlConnection = $ugouvEntityManager->getConnection();
+            $this->initializeConnections();
 
             $tables = $this->getAllTableNames();
 //            dd($tables);
             $tableCount = 1;
+
             foreach ($tables as $table) {
                 $tableName = $table['TABLE_NAME'];
-                $tableName = 'ua_t_facturefrsdet';
+//                $tableName='us_modules_dossiers';
 
                 $output->writeln("$tableCount Processing table: $tableName");
-
-                $moreData = true;
-                $limit = 200;
                 $tableCount++;
+                $moreData = true;
+                $limit = 20000;
 
                 while ($moreData) {
+
                     $data = $this->fetchUnsynchronizedData($tableName, $limit);
                     if (!empty($data)) {
                         $primaryKey = $this->getIdOrPrimaryKey($tableName);
+
                         if (!$primaryKey) {
                             throw new \Exception("Table $tableName does not contain an 'id' column or primary key.");
                         }
 
+                        $columnIds = $this->extractPrimaryKeyValues($data, $primaryKey);
+//                        dd($columnIds);
                         $this->upsertDataIntoTable($data, $tableName, $primaryKey);
                         $this->flagDataAsSynchronized($tableName, $primaryKey, $data);
                     } else {
                         $moreData = false;
                     }
                 }
+                gc_collect_cycles();
             }
 
             $output->writeln('Data synchronized successfully!');
+            $this->updateSyncInfo($synchronisation, 'success', 'Synchronization completed successfully.');
             return Command::SUCCESS;
+
         } catch (\Exception $e) {
-            $output->writeln('An error occurred: ' . $e->getMessage());
+            $output->writeln('Error with table ' . ($tableName ?: 'N/A') . ': ' . $e->getMessage());
+            $this->handleSynchronizationError($synchronisation, $tableName, $e);
             return Command::FAILURE;
         }
     }
 
-    private function getAllTableNames(): array
-    {
-        $sql = "SELECT TABLE_NAME FROM information_schema.tables
-                WHERE TABLE_TYPE = 'BASE TABLE'
-                AND TABLE_SCHEMA = 'ugouv'
-                AND TABLE_NAME NOT IN ('user_created_id','_biomed','_biomed_14_09_22','_biomed_14_09_22_(2)','_biomed_15_09_22_mod','umouvement_antenne_' )";
-
-        $stmt = $this->sqlServerConnection->prepare($sql);
-        return $stmt->executeQuery()->fetchAllAssociative();
-//        dd($stmt->executeQuery()->fetchAllAssociative());
-    }
 
     private function fetchUnsynchronizedData(string $tableName, int $limit): array
     {
-        $sql = "SELECT * FROM $tableName WHERE flag_synchronisation_locale = 0 OR flag_synchronisation_locale IS NULL ";
+        $sql = "SELECT * FROM $tableName WHERE flag_synchronisation_locale = 0 OR flag_synchronisation_locale IS NULL LIMIT $limit";
         $stmt = $this->mysqlConnection->prepare($sql);
         return $stmt->executeQuery()->fetchAllAssociative();
     }
